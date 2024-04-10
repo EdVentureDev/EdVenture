743dad86c53a0e914040453e721401b5b180e785
harshitshukla-dev <hershitshukla35@gmail.com>
2024-04-11 00:32:01 +0530

    Add PageNotFoundComponent to app.component.ts and app.routes.ts

--- EdVenture-FrontEnd/src/app/app.component.ts
+++ EdVenture-FrontEnd/src/app/app.component.ts
@@ -6,11 +6,12 @@ import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavComponent, HomeComponent, AboutComponent, ContactComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavComponent, HomeComponent, AboutComponent, ContactComponent, PageNotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
