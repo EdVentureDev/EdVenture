ae12843c239fe192196283ec625a00dcadf3135c
harshitshukla-dev <hershitshukla35@gmail.com>
2024-04-10 18:32:06 +0000

    11 Apr 2024, 00:02

--- EdVenture-FrontEnd/src/app/app.component.ts
+++ EdVenture-FrontEnd/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 
+// Importing the Components
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NavComponent, HomeComponent, AboutComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
