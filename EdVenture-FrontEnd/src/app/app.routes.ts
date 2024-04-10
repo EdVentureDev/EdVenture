9eb0f13a23e3026a949162edbf93115b054605a6
harshitshukla-dev <hershitshukla35@gmail.com>
2024-04-10 18:38:19 +0000

    11 Apr 2024, 00:08

--- EdVenture-FrontEnd/src/app/app.routes.ts
+++ EdVenture-FrontEnd/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 
 // Importing the Components
+import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 
-export const routes = [
-    { path: '', component: HomeComponent, data: { title: 'EdVenture | Home' } },
-    { path: 'about', component: AboutComponent, data: { title: 'EdVenture | About' } },
-    { path: 'contact', component: ContactComponent, data: { title: 'EdVenture | Contact' } }
+export const routes: Routes = [
+  { title: 'EdVenture | Home', path: '', component: HomeComponent },
+  { title: 'EdVenture | About', path: 'about', component: AboutComponent },
+  { title: 'EdVenture | Contact', path: 'contact', component: ContactComponent }
 ];
