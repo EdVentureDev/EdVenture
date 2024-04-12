ff9ce557a13195f05e2e029fd59247872d730e0f
Tamanna <tamannamalik11a25@gmail.com>
2024-04-13 04:53:36 +0530

    Add resources component to app component imports and HTML

--- EdVenture-FrontEnd/src/app/app.component.ts
+++ EdVenture-FrontEnd/src/app/app.component.ts
@@ -9,11 +9,13 @@ import { ContactComponent } from './components/contact/contact.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ForgetpageComponent } from './components/forgetpage/forgetpage.component';
+import { ResourcesComponent } from './components/resources/resources.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavComponent, HomeComponent, AboutComponent, ContactComponent, PageNotFoundComponent, FooterComponent,ForgetpageComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavComponent, HomeComponent, AboutComponent, ContactComponent, PageNotFoundComponent, FooterComponent,ForgetpageComponent, ResourcesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
