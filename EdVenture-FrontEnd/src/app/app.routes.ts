fbb701bf6b11cc64e566b347b0ba4b0a2d4a30a7
Trishank <itstrishank@gmail.com>
2024-04-13 10:36:34 +0530

    Fixed Login

--- EdVenture-FrontEnd/src/app/app.routes.ts
+++ EdVenture-FrontEnd/src/app/app.routes.ts
@@ -9,7 +9,7 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ForgetpageComponent } from './components/forgetpage/forgetpage.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DashboardCommunityComponent } from './components/dashboard/dashboard-community/dashboard-community.component';
 
 
 export const routes: Routes = [
@@ -20,7 +20,7 @@ export const routes: Routes = [
   { title: 'EdVenture | Signup', path: 'signup', component: SignupComponent },
   { title: 'EdVenture | Login', path: 'login', component: LoginComponent},
   { title: 'EdVenture | Forget Password', path: 'forgetpage', component: ForgetpageComponent},
-  { title: 'EdVenture | Dashboard', path: 'dashboard', component: DashboardComponent},
+  { title: 'EdVenture | Dashboard', path: 'dashboard', component: DashboardCommunityComponent},
   
 
   { title: 'EdVenture | Page Not Found', path: '**', component: PageNotFoundComponent },
--- EdVenture-FrontEnd/src/app/components/dashboard/dashboard-community/dashboard-community.component.ts
+++ EdVenture-FrontEnd/src/app/components/dashboard/dashboard-community/dashboard-community.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DashboardNavComponent } from '../dashboard-nav/dashboard-nav.component';
 
 @Component({
   selector: 'app-dashboard-community',
   standalone: true,
-  imports: [],
+  imports: [DashboardNavComponent],
   templateUrl: './dashboard-community.component.html',
   styleUrl: './dashboard-community.component.css'
 })
